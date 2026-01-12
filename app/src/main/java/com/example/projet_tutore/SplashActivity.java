0d189676c4e50cb2686d70c3afa03c2f16bf70b1
Tigason509 <saintaubinreginald@gmail.com>
2026-01-12 21:44:49 +0100

    Ajout du splashActivity

---
+++ app/src/main/java/com/example/projet_tutore/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.projet_tutore;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000; // 2 secondes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // IMPORTANT
+        }, SPLASH_DURATION);
+    }
+}
