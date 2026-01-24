e8279a362e3a4bcd5c4259638dff986412324eb6
Tigason509 <saintaubinreginald@gmail.com>
2026-01-24 13:30:51 +0100

    Tentative de gestion de l'authentification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.security:security-crypto:1.1.0-alpha06") // Gerer l'encryptage des mdps
+
 }
