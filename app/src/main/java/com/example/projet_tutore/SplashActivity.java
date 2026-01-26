e0c341a4be36e87b46b0c2f01e7175d33c89e9c0
Tigason509 <saintaubinreginald@gmail.com>
2026-01-26 16:31:04 +0100

    SplashActivity.java

--- app/src/main/java/com/example/projet_tutore/SplashActivity.java
+++ app/src/main/java/com/example/projet_tutore/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
-            finish(); // IMPORTANT
+            finish();
         }, SPLASH_DURATION);
     }
 }
