d33b6b7b7e25bce70ef561859d54c3b5b060daf8
Tigason509 <saintaubinreginald@gmail.com>
2026-01-26 14:41:11 +0100

    Lancement du login dans l'application

--- app/src/main/java/com/example/projet_tutore/SplashActivity.java
+++ app/src/main/java/com/example/projet_tutore/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
             finish(); // IMPORTANT
         }, SPLASH_DURATION);
